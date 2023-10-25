79ec74ccb37832492e344124a64586ec55da1221
stanwong <stanwong178@gmail.com>
2023-10-25 15:14:42 +0800

    update hw5

--- hw2/source/main.cpp
+++ hw2/source/main.cpp
@@ -22,6 +22,7 @@
 #include "stm32l475e_iot01_tsensor.h"
 #include "stm32l475e_iot01_hsensor.h"
 #include "stm32l475e_iot01_psensor.h"
+
 #include "stm32l475e_iot01_magneto.h"
 #include "stm32l475e_iot01_gyro.h"
 #include "stm32l475e_iot01_accelero.h"
