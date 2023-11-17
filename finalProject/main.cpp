c2d9851cfd202115ca5a843d292a9fecca3dc026
stanwong <stanwong178@gmail.com>
2023-11-17 13:45:36 +0800

    swipe detection done

--- finalProject/main.cpp
+++ finalProject/main.cpp
@@ -39,165 +39,53 @@
 
 /* Includes */
 #include "mbed.h"
-// #include "HTS221Sensor.h"
-// #include "LPS22HBSensor.h"
-// #include "LSM6DSLSensor.h"
-
-#ifdef TARGET_DISCO_L475VG_IOT01A
+#include <tof_gestures.h>
+#include <tof_gestures_SWIPE_1.h>
 #include "VL53L0X.h"
-
-#else // Nucleo-XXX + X-Nucleo-IKS01A2 or SensorTile
-
-#ifdef TARGET_NUCLEO_L476RG
-#define TARGET_SENSOR_TILE   // comment out to use actual NUCLEO-L476RG instead of SensorTile
-#endif
-
-#include "LSM303AGRMagSensor.h"
-#include "LSM303AGRAccSensor.h"
-
-#endif
-
 
 /* Retrieve the composing elements of the expansion board */
 
 /* Interface definition */
-#ifdef TARGET_DISCO_L475VG_IOT01A
 static DevI2C devI2c(PB_11,PB_10);
-#else // X-Nucleo-IKS01A2 or SensorTile
-#ifdef TARGET_SENSOR_TILE
-#define SPI_TYPE_LPS22HB   LPS22HBSensor::SPI3W
-#define SPI_TYPE_LSM6DSL   LSM6DSLSensor::SPI3W
-SPI devSPI(PB_15, NC, PB_13);  // 3-wires SPI on SensorTile  
-static Serial ser(PC_12,PD_2); // Serial with SensorTile Cradle Exp. Board + Nucleo   
-#define printf(...) ser.printf(__VA_ARGS__)     
-#else  // Nucleo-XXX + X-Nucleo-IKS01A2 
-static DevI2C devI2c(D14,D15);
-#endif
-#endif
-
-// /* Environmental sensors */
-// #ifdef TARGET_SENSOR_TILE
-// static LPS22HBSensor press_temp(&devSPI, PA_3, NC, SPI_TYPE_LPS22HB); 
-// #else  // Nucleo-XXX + X-Nucleo-IKS01A2 or B-L475E-IOT01A2
-// static LPS22HBSensor press_temp(&devI2c);
-// static HTS221Sensor hum_temp(&devI2c);
-// #endif
-
-/* Motion sensors */
-// #ifdef TARGET_DISCO_L475VG_IOT01A
-// // static LSM6DSLSensor acc_gyro(&devI2c,LSM6DSL_ACC_GYRO_I2C_ADDRESS_LOW,PD_11); // low address
-// // static LIS3MDL magnetometer(&devI2c);
-// #else // X-NUCLEO-IKS01A2 or SensorTile
-// #if defined (TARGET_SENSOR_TILE)
-// static LSM6DSLSensor acc_gyro(&devSPI,PB_12, NC, PA_2, SPI_TYPE_LSM6DSL); 
-// static LSM303AGRMagSensor magnetometer(&devSPI, PB_1);
-// static LSM303AGRAccSensor accelerometer(&devSPI, PC_4);
-// #else
-// static LSM6DSLSensor acc_gyro(&devI2c,LSM6DSL_ACC_GYRO_I2C_ADDRESS_HIGH,D4,D5); // high address
-// static LSM303AGRMagSensor magnetometer(&devI2c);
-// static LSM303AGRAccSensor accelerometer(&devI2c);
-// #endif
-// #endif
 
 /* Range sensor - B-L475E-IOT01A2 only */
-#ifdef TARGET_DISCO_L475VG_IOT01A
 static DigitalOut shutdown_pin(PC_6);
-static VL53L0X range(&devI2c, &shutdown_pin, PC_7);
-#endif
+static VL53L0X VL53L0X(&devI2c, &shutdown_pin, PC_7);
 
 /* Simple main function */
 int main() {
-  uint8_t id;
-  float value1, value2;
-//  char buffer1[32], buffer2[32];
-  int32_t axes[3];
-  
-  
-// #ifdef TARGET_DISCO_L475VG_IOT01A
-  range.init_sensor(VL53L0X_DEFAULT_ADDRESS);
-// #else // X-NUCLEO-IKS01A2 or SensorTile
-//   accelerometer.init(NULL);
-// #endif/
-
-  /* Enable all sensors */
-// #ifndef TARGET_SENSOR_TILE  
-//   hum_temp.enable();
-// #endif  
-//   press_temp.enable();
-// #ifndef TARGET_DISCO_L475VG_IOT01A // X-NUCLEO-IKS01A2
-//   magnetometer.enable();
-//   accelerometer.enable();
-// #endif
-//   acc_gyro.enable_x();
-//   acc_gyro.enable_g();
-  
-  printf("\033[2J\033[20A");
-  printf ("\r\n--- Starting new run ---\r\n\r\n");
-
-// #ifndef TARGET_SENSOR_TILE
-//   hum_temp.read_id(&id);
-//   printf("HTS221  humidity & temperature    = 0x%X\r\n", id);
-// #endif  
-//   press_temp.read_id(&id);
-//   printf("LPS22HB pressure & temperature    = 0x%X\r\n", id);
-//   magnetometer.read_id(&id);
-// #ifdef TARGET_DISCO_L475VG_IOT01A
-//   printf("LIS3MDL magnetometer              = 0x%X\r\n", id);
-// #else // X-NUCLEO-IKS01A2 or SensorTile
-//   printf("LSM303AGR magnetometer            = 0x%X\r\n", id);
-//   accelerometer.read_id(&id);
-//   printf("LSM303AGR accelerometer           = 0x%X\r\n", id); 
-// #endif
-//   acc_gyro.read_id(&id);
-//   printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
-  
-  printf("\n\r--- Reading sensor values ---\n\r"); ;
- 
-  while(1) {
-    printf("\r\n");
-
-// #ifndef TARGET_SENSOR_TILE    
-//     hum_temp.get_temperature(&value1);
-//     hum_temp.get_humidity(&value2);
-//     printf("HTS221:  [temp] %.2f C, [hum]   %.2f%%\r\n", value1, value2);
-// #endif    
-//     value1=value2=0;    
-//     press_temp.get_temperature(&value1);
-//     press_temp.get_pressure(&value2);
-//     printf("LPS22HB: [temp] %.2f C, [press] %.2f mbar\r\n", value1, value2);
-
-//     printf("---\r\n");
-
-//     magnetometer.get_m_axes(axes);
-// #ifdef TARGET_DISCO_L475VG_IOT01A
-//     printf("LIS3MDL [mag/mgauss]:    %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-// #else // X-NUCLEO-IKS01A2 or SensorTile
-//     printf("LSM303AGR [mag/mgauss]:  %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-//     accelerometer.get_x_axes(axes);
-//     printf("LSM303AGR [acc/mg]:      %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);    
-// #endif    
-
-//     acc_gyro.get_x_axes(axes);
-//     printf("LSM6DSL [acc/mg]:        %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-
-//     acc_gyro.get_g_axes(axes);
-//     printf("LSM6DSL [gyro/mdps]:     %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-
-#ifdef TARGET_DISCO_L475VG_IOT01A
+    // Gesture structure.
+    Gesture_SWIPE_1_Data_t gestureSwipeData;
+    // Range value
     uint32_t distance;
-    int status = range.get_distance(&distance);
-    if (status == VL53L0X_ERROR_NONE) {
-        printf("VL53L0X [mm]:            %6ld\r\n", distance);
-    } else {
-        printf("VL53L0X [mm]:                --\r\n");
-    }
-#endif
+    uint16_t detected_num = 0;
+  
+    VL53L0X.init_sensor(VL53L0X_DEFAULT_ADDRESS);
+    tof_gestures_initSWIPE_1(&gestureSwipeData);
+  
+    printf("\n\r--- Reading sensor values ---\n\r"); ;
+ 
+    while(1) {
+        int gesture_code;
+        int status;
 
-#if defined (TARGET_SENSOR_TILE)  
-    printf("\033[7A");
-#else    
-    printf("\033[8A");
-#endif
-    ThisThread::sleep_for(5s);
-  }
+        status = VL53L0X.get_distance(&distance);
+        if (status != VL53L0X_ERROR_NONE) {
+            distance = 1200;   
+        }
+        gesture_code = tof_gestures_detectSWIPE_1(distance, &gestureSwipeData);
+        switch(gesture_code)
+        {
+            case GESTURES_SINGLE_SWIPE:
+                detected_num ++;
+                break;
+            default:
+            break;
+        }
+        printf("VL53L0X [mm]:            %6ld, detected: %d \r\n", distance, detected_num);
+
+        printf("\033[8A");
+        
+        ThisThread::sleep_for(10);
+    }
 }
