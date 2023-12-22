a7e6af8ecf29a81fc2ff23ae95c0e0c1b4294a33
stanwong <stanwong178@gmail.com>
2023-12-22 10:52:46 +0800

    ver1.0

--- finalProject/rpi/server.py
+++ finalProject/rpi/server.py
@@ -5,6 +5,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 CORS(app)
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
 
@@ -15,12 +16,20 @@ def index():
 
 @app.route("/stm32_data", methods=["GET"])
 def stm32_data():
-    # data = request.json
-    data = request.args.get('data')
+    data = request.args.get("data")
     print("[backend]:", data)
-    # socketio.emit("stm32_data", data)  # Emitting to browser clients
-    socketio.emit('stm32_data', {'data': data})
+    socketio.emit("stm32_data", {"data": data})
     return "Data received", 200
+
+
+@socketio.on("disconnect")
+def test_disconnect():
+    print("[backend]: client disconnect")
+
+
+@socketio.on("message")
+def handle_message(data):
+    print("[backend]: received message: " + data)
 
 
 if __name__ == "__main__":
